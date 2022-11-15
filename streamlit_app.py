696366cdcdf9e4ecc4fb111a73ed0184f4e309fb
Ganga M C <117728561+Gangamc@users.noreply.github.com>
2022-11-15 20:25:59 +0530

    Closing the snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,9 +52,11 @@ def get_fruit_load_list():
         return my_cur.fetchall()
     
 # Add a button to load the list
-if streamlit.button('Get Fruit Load List'):
+streamlit.header('View our FruitList and Add your Favorites!')
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
     
 
@@ -69,6 +71,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
         
 
